60b872f24fbeff78d5db6528bd7eb98e9d1912c3
Chris Baxter <Chris.Baxter@shaw.ca>
2012-11-18 12:14:24 -0700

    Added initial extension methods.

---
+++ src/Common/EnumerableExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Spark.Infrastructure
+{
+    /// <summary>
+    /// Extension methods of <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Cast <paramref name="source"/> as an <see cref="IList{T}"/> if possible; otherwise create a new <see cref="List{T}"/> wrapper.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">The <see cref="IEnumerable{T}"/> to cast as an <see cref="IList{T}"/> or create a <see cref="List{T}"/> from.</param>
+        public static IList<T> AsList<T>(this IEnumerable<T> source)
+        {
+            return source == null ? null : source as IList<T> ?? new List<T>(source);
+        }
+
+        /// <summary>
+        /// Cast <paramref name="source"/> as an <see cref="IReadOnlyList{T}"/> if possible; otherwise create a new <see cref="ReadOnlyCollection{T}"/> wrapper.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">The <see cref="IEnumerable{T}"/> to cast as an <see cref="IReadOnlyList{T}"/> or create a <see cref="ReadOnlyCollection{T}"/> from.</param>
+        public static IReadOnlyList<T> AsReadOnly<T>(this IEnumerable<T> source)
+        {
+            return source == null ? null : source as IReadOnlyList<T> ?? new ReadOnlyCollection<T>(source.AsList());
+        }
+
+        /// <summary>
+        /// Returns the elements of the specified sequence or an empty sequence if <paramref name="source"/> is null.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source.</typeparam>
+        /// <param name="source">The sequence to return an empty enumerable for if null.</param>
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> source)
+        {
+            return source ?? Enumerable.Empty<T>();
+        }
+    }
+}
---
+++ src/Common/ObjectExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spark.Infrastructure
+{
+    /// <summary>
+    /// Extension methods of <see cref="Object"/>.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Creates a new <see cref="Array"/> containing <paramref name="item"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <paramref name="item"/>.</typeparam>
+        /// <param name="item">The singleton element to be added to the <see cref="Array"/>.</param>
+        public static T[] ToArray<T>(this T item)
+        {
+            return new[] { item };
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="List{T}"/> containing <paramref name="item"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of <paramref name="item"/>.</typeparam>
+        /// <param name="item">The singleton element to be added to the <see cref="List{T}"/>.</param>
+        public static List<T> ToList<T>(this T item)
+        {
+            return new List<T> { item };
+        }
+    }
+}
