4fd9710b7b6330db0e87d4debb8554caee13db99
CBaxter <Chris.Baxter@shaw.ca>
2014-06-26 11:54:09 -0600

    Ensure TypeLocator always references Core assembly.
    NOTE: Otherwise Timeout event handlers may not be discovered.

---
+++ src/Core/CoreAssembly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Spark
+{
+    /// <summary>
+    /// Core assembly reference/version.
+    /// </summary>
+    internal static class CoreAssembly
+    {
+        /// <summary>
+        /// The core assembly reference.
+        /// </summary>
+        public static readonly Assembly Reference = typeof(CoreAssembly).Assembly;
+
+        /// <summary>
+        /// The core assembly version.
+        /// </summary>
+        public static readonly Version Version = Reference.GetName().Version;
+    }
+}
